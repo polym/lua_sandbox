2138edc1438947cba30c64611d4b189816b0becf
hongbo.mo <hongbo.mo@upai.com>
2017-10-18 10:09:58 +0800

    update lua repo

--- cmake/externals.cmake
+++ cmake/externals.cmake
@@ -64,8 +64,8 @@ else()
     set(LUA_PROJECT "lua-5_1_5")
     externalproject_add(
         ${LUA_PROJECT}
-        GIT_REPOSITORY https://github.com/lua/lua.git
-        GIT_TAG v5_1_1
+        GIT_REPOSITORY https://github.com/polym/lua.git
+        GIT_TAG 733e6e41b33c08dc3bd21ab49421bd139a52c17a
         CMAKE_ARGS ${SANDBOX_CMAKE_ARGS}
         INSTALL_ARGS ${INST_ARGS}
     )
